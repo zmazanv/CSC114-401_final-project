d8a991cc5b06ebcced88644ec2d896b2ba1db24a
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-07-21 14:48:54 -0400

    read_csv_sample: Remove 'using namespace std;'

--- read_csv_sample.cpp
+++ read_csv_sample.cpp
@@ -5,20 +5,18 @@
 #include <string>
 #include <sstream>
 
-using namespace std;
-
 const int STUDENTNUM = 2;
 const int COURSENUM = 5;
 
 int main()
 {
-	string fname;
-	cout << "Enter the file name: ";
-	cin >> fname;
-	string line, word;
+	std::string fname;
+	std::cout << "Enter the file name: ";
+	std::cin >> fname;
+	std::string line, word;
 	int score[STUDENTNUM][COURSENUM];
 
-	fstream file(fname, ios::in);
+	std::fstream file(fname, std::ios::in);
 	if (file.is_open())
 	{
 		int rw = 0;
@@ -28,7 +26,7 @@ int main()
 			//row.clear();
 			int cl = 0;
 
-			stringstream str(line);
+			std::stringstream str(line);
 
 			while (getline(str, word, ','))
 			{
@@ -40,16 +38,16 @@ int main()
 		}
 	}
 	else
-		cout << "Could not open the file\n";
+		std::cout << "Could not open the file\n";
 
 
 	for (int i = 0; i < STUDENTNUM; i++)
 	{
 		for (int j = 0; j < COURSENUM; j++)
 		{
-			cout << score[i][j] << " ";
+			std::cout << score[i][j] << " ";
 		}
-		cout << "\n";
+		std::cout << "\n";
 	}
 
 	return 0;
